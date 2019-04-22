36c0ba67eb65d68286217fbb89da03f608f7dfdf
pedro-rodalia <pedro.rodalia@gmail.com>
2019-04-22 18:30:48 +0200

    added custom settings table and methods detailed explanation

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ export default function cachePlugin (instance, customSettings) {
 
     /* CACHE DEFAULT SETTINGS */
     const defaultSettings = {
-      methods: ['post', 'get'],
+      methods: ['get'],
       endpoints: [],
       garbageColector: true, // Remove old calls automatically
       ttl: 60 // Default cache time to live in seconds
